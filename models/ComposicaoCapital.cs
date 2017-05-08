317f860267e321af8b8777bc765a9a83db7dc728
Lucas Lemos Bataglião <lucas.batagliao@consinco.com.br>
2017-05-08 10:39:10 -0300

    estrutura objeto ComposicaoCapital

---
+++ models/ComposicaoCapital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bvmfscrapper.models
+{
+    public class ComposicaoCapital
+    {
+        public Periodo Periodo { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Multiplicador { get; set; }
+
+        public decimal PrefernciaisCirculantes { get; set; }
+        public decimal OrdinariasCirculantes { get; set; }
+        public decimal PreferenciaisTesouraria { get; set; }
+        public decimal OrdinariasTesouraria { get; set; }
+    }
+}
